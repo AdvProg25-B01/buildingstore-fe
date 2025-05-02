fc06ca8e4d978004e59257d6862ebccf7031efaf
sopigoo <shofinadhifa24@gmail.com>
2025-05-02 18:52:53 +0700

    feat: app routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import AdminDashboard from './pages/AdminDashboard';
+import KasirDashboard from './pages/KasirDashboard';
+import CreateUser from './pages/CreateUser';
+import LandingPage from './pages/LandingPage';
 
 function App() {
+  const userRole = localStorage.getItem('role');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        🚀 Deployment test from Building Store B01! Vercel should redeploy now.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        {userRole === 'ADMIN' && (
+          <>
+            <Route path="/admin-dashboard" element={<AdminDashboard />} />
+            <Route path="/create-user" element={<CreateUser />} />
+          </>
+        )}
+        {userRole === 'KASIR' && <Route path="/kasir-dashboard" element={<KasirDashboard />} />}
+      </Routes>
+    </Router>
   );
 }
 

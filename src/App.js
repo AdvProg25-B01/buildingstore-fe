a34983b8835d84d3a52a373473049204af3ae81b
sopigoo <shofinadhifa24@gmail.com>
2025-05-23 10:45:55 +0700

    chore: fix routing

--- src/App.js
+++ src/App.js
@@ -1,32 +1,53 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import AdminDashboard from './pages/AdminDashboard';
-import KasirDashboard from './pages/KasirDashboard';
-import CreateUser from './pages/CreateUser';
-import LandingPage from './pages/LandingPage';
+import Navbar           from './components/Navbar';
+import Footer           from './components/Footer';
+import LandingPage      from './pages/LandingPage';
+import Login            from './pages/Login';
+import Register         from './pages/Register';
+import AdminDashboard   from './pages/AdminDashboard';
+import KasirDashboard   from './pages/KasirDashboard';
+import CreateUser       from './pages/CreateUser';
+import ProtectedRoute   from './components/ProtectedRoute';
 
 function App() {
-  const userRole = localStorage.getItem('role');
-
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
+        {/* Public */}
+        <Route path="/"        element={<LandingPage />} />
+        <Route path="/login"   element={<Login />} />
         <Route path="/register" element={<Register />} />
-        {userRole === 'ADMIN' && (
-          <>
-            <Route path="/admin-dashboard" element={<AdminDashboard />} />
-            <Route path="/create-user" element={<CreateUser />} />
-          </>
-        )}
-        {userRole === 'KASIR' && <Route path="/kasir-dashboard" element={<KasirDashboard />} />}
+
+        {/* Admin-only */}
+        <Route
+          path="/admin-dashboard"
+          element={
+            <ProtectedRoute allowedRoles={['ADMIN']}>
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/create-user"
+          element={
+            <ProtectedRoute allowedRoles={['ADMIN']}>
+              <CreateUser />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Kasir-only */}
+        <Route
+          path="/kasir-dashboard"
+          element={
+            <ProtectedRoute allowedRoles={['KASIR']}>
+              <KasirDashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
       <Footer />
     </Router>

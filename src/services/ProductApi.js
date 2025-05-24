b81a1a198f55b39d63ffa197b2a417a0cb78ccd7
sopigoo <shofinadhifa24@gmail.com>
2025-05-25 06:01:13 +0700

    chore: update api product

--- src/services/ProductApi.js
+++ src/services/ProductApi.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080/product';
+const API_PRODUCT_URL = process.env.REACT_APP_API_TRANSACTION_URL || 'http://localhost:8081';
+const BASE_PRODUCT_URL = `${API_PRODUCT_URL}/product`;
 
-export const getAllProducts = () => axios.get(`${BASE_URL}/list`);
-export const getProductByName = (name) => axios.get(`${BASE_URL}/${name}`);
-export const createProduct = (data) => axios.post(`${BASE_URL}/create`, data);
-export const updateProduct = (name, data) => axios.put(`${BASE_URL}/edit/${name}`, data);
-export const deleteProduct = (name) => axios.delete(`${BASE_URL}/delete/${name}`);
+export const getAllProducts = () => axios.get(`${BASE_PRODUCT_URL}/list`);
+export const getProductByName = (name) => axios.get(`${BASE_PRODUCT_URL}/${name}`);
+export const createProduct = (data) => axios.post(`${BASE_PRODUCT_URL}/create`, data);
+export const updateProduct = (name, data) => axios.put(`${BASE_PRODUCT_URL}/edit/${name}`, data);
+export const deleteProduct = (name) => axios.delete(`${BASE_PRODUCT_URL}/delete/${name}`);

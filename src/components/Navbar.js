a084f63a4271bfac3d4b293e5e7dcd913946a394
Edmond Christian <146692883+EdPootis@users.noreply.github.com>
2025-05-25 18:07:13 +0700

    Update Navbar.js (Removed merge error)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -124,7 +124,6 @@ const Navbar = () => {
 
                 <li>
                   <button
-                  <button 
                     onClick={handleLogout}
                     className="bg-blue-600 hover:bg-blue-500 text-white px-4 py-2 rounded-lg transition duration-300"
                   >

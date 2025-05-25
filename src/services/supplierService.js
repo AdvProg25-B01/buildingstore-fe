000dd87113101c25f6afbec995dd9933d7afaaa9
Edmond Christian <edmond.chr@gmail.com>
2025-05-25 12:45:19 +0700

    [REFACTOR] Changed enviroment variable name

--- src/services/supplierService.js
+++ src/services/supplierService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8081';
+const API_URL = process.env.REACT_APP_SUPPLIER_API_URL;
 
 const supplierService = {
   getAllSuppliers: async () => {
